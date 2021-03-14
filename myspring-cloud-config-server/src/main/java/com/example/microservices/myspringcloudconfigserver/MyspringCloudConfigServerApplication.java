05d2e3972d84cb39ba48e8263cf516ca6a1d22c9
Ashish Anil Kirpan <ashishkirpan@gmail.com>
2021-03-14 17:25:23 +0530

    adding microservice cloud-config-server

---
+++ myspring-cloud-config-server/src/main/java/com/example/microservices/myspringcloudconfigserver/MyspringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices.myspringcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringCloudConfigServerApplication.class, args);
+	}
+
+}
